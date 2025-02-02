cda0d189aeb54ddc24f82abd98ce3e534b415303
boozie4 <90736536+boozie4@users.noreply.github.com>
2025-02-02 09:21:34 -0600

    Web-Server push

---
+++ restful-api-mysql/src/routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+// const logger = require('erik');
+const bodyParser = require('body-parser');
+
+const tasksRoutes = require('./routes/tasks.routes');
+const middleware = require('./middleware/errors.middleware');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const logLevel = process.env.LOG_LEVEL || 'dev';
+
+// Middleware - logs server requests to console
+app.use(logger(logLevel));
+
+// Middleware - parses incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// ***********************************
+// ROUTE_HANDLING MIDDLWARE FUNCTIONS
+// ***********************************
+
+// Handle routes for tasks.
+app.use('tasks', tasksRoutes); // http://localhost:3000/tasks
+
+// Handle 404 requests
+//app.use(middleware.error404);
+
+// Handle 500 requests - applies mostly to live services
+//app.use(middleware.error500);
+
+// listen on server port
+app.listen(port, () => {
+    console.log('Running on port: ${port} ...');
+})
